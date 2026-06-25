36deb5325f9b9ff68d9c37d7bb93e8079f61edc0
Prince yadav <pkb705249@gmail.com>
2026-06-25 13:20:01 +0530

    Create DiameterOfBT.java

---
+++ Week_4/Day_4/DiameterOfBT.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int dia=0;
+    private int Height(TreeNode root){
+        if(root==null) return 0;
+        int left=Height(root.left);
+        int right=Height(root.right);
+        dia=Math.max(dia,left+right);
+        return 1+Math.max(left,right);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        dia=0;
+        Height(root);
+        return dia;
+        
+    }
+}
